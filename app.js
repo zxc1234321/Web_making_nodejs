a9a7c8dc5cd2b42f14c969f5fed7ace0451f2df9
zxc1234321 <spcle2412@gmail.com>
2023-11-10 16:11:16 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+//app.get('/main', function (req, res) {
+//    res.sendFile(__dirname+'/main.html');
+//});
+
+//app.get('/test', function (req, res) {
+//    res.sendFile(__dirname+'/test.html');
+//});
+
+const PORT = 8080;
+app.listen(PORT, function () {
+    console.log('Listening on port: ', PORT);
+});
