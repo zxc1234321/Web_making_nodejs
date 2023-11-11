0a77649367817e9cecaa4af9775bcb5f5ed7c4ea
zxc1234321 <spcle2412@gmail.com>
2023-11-11 11:46:50 +0900

    modified by zxc1234321

--- app.js
+++ app.js
@@ -11,11 +11,7 @@ app.get('/test', function (req, res) {
 */
 const PORT = 8080;
 app.listen(PORT, function () {
-<<<<<<< HEAD
     console.log('Server Init');
-=======
-    console.log('Server Start')
->>>>>>> bd5c886022e1ead703b63a5f4184ea4bc4d54f5c
     console.log('Listening on port: ', PORT);
-    
+
 });
